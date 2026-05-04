995fce4e8ae30dc8494627e4d09f96f5a4b92d15
Shubhamrane-890 <dummy@gmail.com>
2026-05-04 20:29:40 +0000

    day-23

--- client/src/app/mediconnect/models/Appointment.ts
+++ client/src/app/mediconnect/models/Appointment.ts
@@ -1,33 +1,28 @@
+
 import { Clinic } from './Clinic';
 import { Patient } from './Patient';
 
 export class Appointment {
-  appointmentId: number;
-  patient: Patient;
-  clinic: Clinic;
-  appointmentDate: Date;
-  status: string;
-  purpose: string;
+    appointmentId: number;
+    patient: Patient;
+    clinic: Clinic;
+    appointmentDate: Date;
+    status: string;
+    purpose: string;
 
-  constructor(
-    appointmentId: number,
-    patient: Patient,
-    clinic: Clinic,
-    appointmentDate: Date,
-    status: string,
-    purpose: string
-  ) {
-    this.appointmentId = appointmentId;
-    this.patient = patient;
-    this.clinic = clinic;
-    this.appointmentDate = appointmentDate;
-    this.status = status;
-    this.purpose = purpose;
-  }
-
-  logAttributes(): void {
-    Object.entries(this).forEach(([key, value]) => {
-      console.log(`${key}:`, value);
-    });
-  }
+    constructor(
+        appointmentId: number,
+        patient: Patient,
+        clinic: Clinic,
+        appointmentDate: Date,
+        status: string,
+        purpose: string
+    ) {
+        this.appointmentId = appointmentId;
+        this.patient = patient;
+        this.clinic = clinic;
+        this.appointmentDate = appointmentDate;
+        this.status = status;
+        this.purpose = purpose;
+    }
 }
--- client/src/app/mediconnect/models/Clinic.ts
+++ client/src/app/mediconnect/models/Clinic.ts
@@ -1,32 +1,26 @@
 import { Doctor } from './Doctor';
 
 export class Clinic {
-  clinicId: number;
-  clinicName: string;
-  location: string;
-  contactNumber: string;
-  establishedYear: number;
-  doctor: Doctor; // Many-to-One relationship (each clinic is associated with one doctor)
+    clinicId: number;
+    clinicName: string;
+    location: string;
+    contactNumber: string;
+    establishedYear: number;
+    doctor: Doctor; // Many-to-One relationship (Each clinic is associated with one doctor)
 
-  constructor(
-    clinicId: number,
-    clinicName: string,
-    location: string,
-    contactNumber: string,
-    establishedYear: number,
-    doctor: Doctor
-  ) {
-    this.clinicId = clinicId;
-    this.clinicName = clinicName;
-    this.location = location;
-    this.contactNumber = contactNumber;
-    this.establishedYear = establishedYear;
-    this.doctor = doctor;
-  }
-
-  logAttributes(): void {
-    Object.entries(this).forEach(([key, value]) => {
-      console.log(`${key}:`, value);
-    });
-  }
+    constructor(
+        clinicId: number,
+        clinicName: string,
+        location: string,
+        contactNumber: string,
+        establishedYear: number,
+        doctor: Doctor
+    ) {
+        this.clinicId = clinicId;
+        this.clinicName = clinicName;
+        this.location = location;
+        this.contactNumber = contactNumber;
+        this.establishedYear = establishedYear;
+        this.doctor = doctor;
+    }
 }
